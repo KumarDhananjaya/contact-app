a1f3b4ac3978c1ae4b67c03f48b7c53319ba760f
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-21 20:58:57 +0530

    Checking browser localStorage availablity

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import "./App.css";
 import Header from "./Header"
 import AddContact from "./AddContact";
@@ -10,21 +11,29 @@ function App() {
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   }
 
   useEffect(() => {
     const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if(retrieveContacts) setContacts(retrieveContacts);
+    if (retrieveContacts) setContacts(retrieveContacts);
   }, []);
+  
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
-
+  // checking the browser localStorage Availibility 
+  // if (typeof localStorage === 'undefined') {
+  //   console.log('localStorage is not available in this browser.');
+  // } else {
+  //   console.log('localStorage is available!');
+  // }
+  
   return (
     <div>
+      
       <Header />
       <AddContact addContactHandler = {addContactHandler}/>
       <ContactList contacts = {contacts} />
