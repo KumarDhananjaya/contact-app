94ebf4e3c1b03246ca1a1682c5aa9683a00f572e
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-21 17:28:08 +0530

    rendering props and added user icon

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import user from "../images/user.png"
 
-const ContactCard = () => {
+const ContactCard = (props) => {
+
+    const {id, name, email} = props.contact;
   return (
-    <div>ContactCard</div>
-  )
+    <div className="item">
+    <img className="ui avatar image" src ={user} alt="user" />
+    <div className="content">
+        <div className="header"> {name}</div>
+        <div> {email}</div>
+        <i style = {{color: "red", margintop: "5px" }} className="trash alternate outline icon"></i>
+    </div>
+</div>  )
 }
 
 export default ContactCard
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,17 +1,12 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
 const ContactList = (props) => {
     console.log(props)
 
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <div className="item">
-                <div className="content">
-                    <div className="header"> {contact.name}</div>
-                    <div> {contact.email}</div>
-                    <i className="trash alternate outline icon"></i>
-                </div>
-            </div>
+            <ContactCard contact = {contact}/>
         )
     })
   return (
