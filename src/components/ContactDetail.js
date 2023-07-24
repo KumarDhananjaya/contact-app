349a910cda295d9d84d62c77d8d87c9e2276d207
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-24 12:53:45 +0530

    completed userdetail component with static data

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,12 +1,21 @@
 import React from "react";
-import user from "../images/user.png";
+import userdetail from "../images/userdetail.png";
 import { Link } from "react-router-dom";
 
 
 const ContactDetail= (props) => {
   return (
     <div className="main">
-        
+        <div className="ui card centered">
+            <div className="image">
+                <img src={userdetail} alt ="user" />
+            </div>
+            <div className="content"> 
+                <div className="header">Kumar</div>
+                <div className="description">Kumar62.shivu@gmail.com</div>
+            </div>
+        </div>
+
     </div>
   );
 };
