500dc45045999ed67629d001cf422ec6c3b4f091
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-24 12:43:22 +0530

    routing thorugh user unique id

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import user from "../images/user.png";
+import { Link } from "react-router-dom";
+
 
 const ContactCard = (props) => {
   const { id, name, email } = props.contact;
@@ -7,8 +9,10 @@ const ContactCard = (props) => {
     <div className="item">
       <img className="ui avatar image" src={user} alt="user" />
       <div className="content">
+        <Link to ={`/conatct/${id}`}>
         <div className="header">{name}</div>
         <div>{email}</div>
+        </Link>
       </div>
       <i
         className="trash alternate outline icon"
