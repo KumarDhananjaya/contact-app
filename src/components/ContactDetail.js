3a3f9b93d64a1f4c2897c06d2f8d0b8c12f977b9
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-24 12:48:14 +0530

    created contact detail component and added new user image to display in this

---
+++ src/components/ContactDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import user from "../images/user.png";
+import { Link } from "react-router-dom";
+
+
+const ContactDetail= (props) => {
+  return (
+    <div className="main">
+        
+    </div>
+  );
+};
+
+export default ContactDetail;
